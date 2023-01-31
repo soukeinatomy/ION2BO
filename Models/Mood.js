08d8d6930db5705e588b42c797f0a0fd0f5371bb
Suki <soukeinatomy@gmail.com>
2023-01-31 01:31:31 -0500

    .

--- Models/Mood.js
+++ Models/Mood.js
@@ -1,21 +1,14 @@
 // import dependencies
 const mongoose = require('./connection')
 
-// import user model for populate
 const User = require('./User')
 
-// destructure the schema and model constructors from mongoose
-const { Schema, model } = mongoose
 
-const exampleSchema = new Schema(
+
+const MoodSchema = new Schema(
 	{
-		title: { type: String, required: true },
-		body: { type: String, required: true },
-        amount: { type: Number, required: true },
-		ready: { type: Boolean, required: true },
-		owner: {
-			type: Schema.Types.ObjectID,
-			ref: 'User',
+	name: {String,
+	help: String // the link to the help resource
 		}
 	},
 	{ timestamps: true }
@@ -26,4 +19,4 @@ const Example = model('Example', exampleSchema)
 /////////////////////////////////
 // Export our Model
 /////////////////////////////////
-module.exports = Example
+module.exports = Mood
