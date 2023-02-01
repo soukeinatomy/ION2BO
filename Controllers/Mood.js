e2f7df024f0a4c4a8615f065152741d2c67b1d4d
Suki <soukeinatomy@gmail.com>
2023-02-01 13:30:50 -0500

    .

--- Controllers/Mood.js
+++ Controllers/Mood.js
@@ -6,25 +6,25 @@ const Mood = require('../Models/Mood')
 // Create router
 const router = express.Router()
 
-// Router Middleware
+//Router Middleware
 // Authorization middleware
 
-// router.get((req, res, next) => {
-// // checking the loggedIn boolean of our session
-// 	if (req.session.loggedIn) {
-// // if they're logged in, go to the next thing(thats the controller)
-// 		next()
-// 	} else {
-// 		// if they're not logged in, send them to the login page
-// 		res.redirect('/auth/login')
-// 	}
-// })
+router.get((req, res, next) => {
+// checking the loggedIn boolean of our session
+	if (req.session.loggedIn) {
+// if they're logged in, go to the next thing(thats the controller)
+		next()
+	} else {
+		// if they're not logged in, send them to the login page
+		res.redirect('/auth/login')
+	}
+})
 
 
 //Render Mood page
 
 router.get ('/', (req, res) =>{
-    res.render ('auth/Mood')
+    res.render ('Mood')
 }
 )
 
